fd856ccc3e53466e4a7ebfe4cd7704bed1d40799
Christopher Webber <cwebberops@gmail.com>
2014-07-14 10:57:10 -0400

    When in doubt, read the code

--- lib/cookbooktracker/cla.rb
+++ lib/cookbooktracker/cla.rb
@@ -7,14 +7,14 @@ module CookbookTracker
         cla_label = 'Signed CLA'
 
         #octokit_client = Octokit::Client.new(:access_token => github_user.token)
-        #octokit_client = Octokit::Client.new(:login => github_user.login, :access_token => github_user.access_token)
+        octokit_client = github_user.api
 
-        github_user.api.auto_paginate = true
-        repos = github_user.api..org_repos('opscode-cookbooks', {:type => 'public'})
+        octokit_client.auto_paginate = true
+        repos = octokit_client.org_repos('opscode-cookbooks', {:type => 'public'})
         @cla = []
         repos.each do |r|
           if r.has_issues?
-            issues = github_user.api.list_issues(r.full_name, {:labels => cla_label})
+            issues = octokit_client.list_issues(r.full_name, {:labels => cla_label})
             @cla << {:repo => r.full_name, :count => issues.count}
           end
         end
